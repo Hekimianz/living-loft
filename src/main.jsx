17900d4f44f4b7ff8fdfcbe47fc2f1b7d57cf7bf
Aram Hekimian <guerrerohek121@gmail.com>
2024-12-30 18:11:30 -0600

    Set up react router and initial file structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home/Home.jsx";
+import Store from "./routes/Store/Store.jsx";
+import About from "./routes/About/About.jsx";
+import Contact from "./routes/Contact/Contact.jsx";
+import "./main.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="store" element={<Store />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
---
+++ src/routes/Store/Store.jsx
@@ -0,0 +1,5 @@
+const Store = () => {
+  return <h1>This is the store page!</h1>;
+};
+
+export default Store;
